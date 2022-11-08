c5bda64e882abb4b624debc79d0b0e7733a07614
Michael Dietrich <michael.dietrich@s2010.tu-chemnitz.de>
2022-11-09 00:11:18 +0100

    refactor

--- utils.py
+++ utils.py
@@ -35,15 +35,15 @@ def recv_game(sock, blocking=True):
         raise socket.error
 
 
-def join_game(addr, nick):
+def join_game(addr, nickname):
     try:
         client_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         client_sock.connect(addr)
-        send_msg(client_sock, addr, 'JOIN ' + nick)
+        send_msg(client_sock, addr, 'JOIN ' + nickname)
         msg, addr = recv_msg(client_sock, blocking=True)
-        print('Antwort vom Server: {addr} msg'.format(addr=addr, msg=msg))
-        if msg == 'OK':
-            return client_sock
+        print('Response from server: {addr} {msg}'.format(addr=addr, msg=msg))
+        if msg.startswith('OK'):
+            return client_sock, msg.split()[1]
         if msg == 'NOPE':
             return 'NOPE'
     except:
