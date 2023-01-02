46a521aed701234b07499b7590b4dc0252703eda
Michael Dietrich <michael.dietrich@s2010.tu-chemnitz.de>
2023-01-02 17:57:48 +0100

    change player speed rate within the object

--- maenneltest.py
+++ maenneltest.py
@@ -14,23 +14,21 @@ class Player:
         self.speed = 3
         self.size = 50
 
-    def move(self, step_time, win_width, win_height):
-        self.rate_x = step_time * self.speed
-        self.rate_y = step_time * self.speed
+    def move(self, step_time, direction_x, direction_y, win_width, win_height):
+        self.rate_x = step_time * direction_x * self.speed
+        self.rate_y = step_time * direction_y * self.speed
+        self.pos_x += self.rate_x
+        self.pos_y += self.rate_y
         if self.rate_x >= 0:  # right
-            self.pos_x += self.rate_x
             if self.pos_x + self.size > win_width:
                 self.pos_x = win_width - self.size
         else:  # left
-            self.pos_x -= self.rate_x
             if self.pos_x < 0:
                 self.pos_x = 0
         if self.rate_y >= 0:  # down
-            self.pos_y += self.rate_y
             if self.pos_y + self.size > win_height:
                 self.pos_y = win_height - self.size
         else:  # up
-            self.pos_y -= self.rate_y
             if self.pos_y < 0:
                 self.pos_y = 0
 
@@ -42,17 +40,17 @@ class Maenneltest:
         self.win_width = WIDTH
         self.win_height = HEIGHT
 
-    def move_player(self, info, step_time):
+    def move_player(self, info, step_time, direction_x, direction_y):
         if isinstance(info, str):
             player = self.players.get(info)
         else:
             player = info
         if player:
-            player.move(step_time, self.win_width, self.win_height)
+            player.move(step_time, direction_x, direction_y, self.win_width, self.win_height)
 
-    def update_players(self):
-        for _, player in self.players.items():
-            self.move_player(player, player.rate_x, player.rate_y)
+    # def update_players(self):
+    #     for _, player in self.players.items():
+    #         self.move_player(player, player.rate_x, player.rate_y)
 
     def add_player(self, nickname):
         if self.players.get(nickname):
